4014f00b3a6f5ffef393f5a7a924c222413938a6
AnthonyGKruger <antman.kruger@gmail.com>
2023-10-12 18:28:40 +0200

    backing up first round work

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,7 +3,6 @@ import Nav from "./Nav";
 const Header = () => {
   return (
     <header className="absolute top-0  z-20 w-full after:absolute after:top-full after:left-0 after:z-30 after:block after:h-px  after:bg-slate-200 lg:border-slate-200  lg:after:hidden">
-      {/*<header className="absolute top-0  z-20 w-full after:absolute after:top-full after:left-0 after:z-30 after:block after:h-px after:w-full after:bg-slate-200 lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden">*/}
       <Nav />
     </header>
   );
